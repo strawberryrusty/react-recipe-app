560fb1b14523dda710564779b2d7610c584ddbaf
Prash Mohan <prashmohanuon@gmail.com>
2020-08-02 22:22:55 +0100

    app.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React!
+          React Cooking App
         </a>
       </header>
     </div>
